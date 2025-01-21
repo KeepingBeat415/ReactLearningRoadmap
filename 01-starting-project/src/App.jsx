1a99abf5c692f18cb258e92783f95a4b7554775f
Ms-BUG <sevenliu18@gmail.com>
2025-01-20 22:21:25 -0500

    250120

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,7 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -7,12 +11,29 @@ function genRandomInt(max) {
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
+  );
+}
+
+// function CoreConcept(props) {
+// {} used to destructure the first parameter of this function
+function CoreConcept({ image, title, description }) {
+  return (
+    // <li>
+    //   <img src={props.image} alt={props.title} />
+    //   <h3>{props.title}</h3>
+    //   <p>{props.description}</p>
+    // </li>
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
@@ -22,6 +43,20 @@ function App() {
       {/* <Header></Header> */}
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            {/* short cut for grab object attributes from list into props */}
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
