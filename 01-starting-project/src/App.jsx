5c82721f98ad7de4164bf1a16db79a7fe6884e0e
Ms-BUG <sevenliu18@gmail.com>
2025-01-24 22:49:12 -0500

    250124

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
-  function handleSelect() {
-    console.log('Hello World - selected');
+  /*
+  const [current state value, state updating function] = useState(initial state value);
+
+  initial state value: stored by React
+  state updating function: updates the stored values & 'tells' react to re-execute the component function in which useState() was called
+  current state value: provide by React may change if the component function is executed again
+  */
+
+  const [selectedTopic, setSelectedTopic] = useState('components'); // React Hook functions must only be called inside of component functions or custom Hooks
+
+  //let tabContent = 'Please click a button';
+
+  function handleSelect(selectedButton) {
+    //console.log(selectedButton);
+    // tabContent = selectedButton;
+    // console.log(tabContent);
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -31,12 +49,19 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
         </section>
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
       </main>
     </div>
   );
