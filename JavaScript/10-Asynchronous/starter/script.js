563007b8db2c83a4ebc9c1a53b7187b21ac4cadb
Ms-BUG <sevenliu18@gmail.com>
2025-03-18 21:58:42 -0400

    250318-1

--- JavaScript/10-Asynchronous/starter/script.js
+++ JavaScript/10-Asynchronous/starter/script.js
@@ -188,3 +188,22 @@ const getCountryDataFun = function (country) {
     })
     .finally(() => {});
 };
+
+// url https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
+
+const whereAmI = function (lat, lng) {
+  fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+  )
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+    })
+    .catch(err => {
+      console.error(`${err.message}`);
+    });
+};
